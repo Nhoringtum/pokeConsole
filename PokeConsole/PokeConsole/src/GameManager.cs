8cd4763e820e75ee5e6118f90ccc7f9f300561b5
Nodeions <ehamida@gaming.tech>
2025-10-04 18:39:10 +0200

    [ADD]
    GameManager singleton

---
+++ PokeConsole/PokeConsole/src/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeConsole.src
+{
+    internal class GameManager
+    {
+        private GameManager() 
+        {
+            Console.WriteLine("aaaaa");
+        }
+
+        private static GameManager _instance;
+        public static GameManager Instance 
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameManager();
+                }
+                return _instance;
+            }
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+
+        private void QuitGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
